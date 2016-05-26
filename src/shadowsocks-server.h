fe4f67dbdc66b10a1d4ef4ae840db94a1a1d9e08
xiaoweime <handsomexiaowei@gmail.com>
2016-05-26 21:34:40 +0800

    support windows server dll

---
+++ src/shadowsocks-server.h
@@ -0,0 +1,20 @@
+
+#ifndef SHADOWSOCKS_SERVER_H_NRTAG2MQ
+#define SHADOWSOCKS_SERVER_H_NRTAG2MQ
+
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+
+int start_ss_server(int argc, char **argv);  
+
+
+#ifdef __cplusplus
+}
+#endif    
+
+#endif /* end of include guard: SHADOWSOCKS_SERVER_H_NRTAG2MQ */ 
